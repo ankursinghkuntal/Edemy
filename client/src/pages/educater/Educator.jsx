1b11b46aed3b044ffc6153aab3d0384763a32531
ankursinghkuntal <ankursinghkuntal361@gmail.com>
2025-02-25 00:40:47 +0530

    first commit

---
+++ client/src/pages/educater/Educator.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educater/Navbar'
+import Sidebar from '../../components/educater/Sidebar'
+import Footer from "../../components/educater/Footer";
+
+const Educator = () => {
+  return (
+    <div className='text-default min-h-screen bg-white'>
+        <Navbar />
+        <div className='flex'>
+          <Sidebar />
+          <div className='flex-1'>
+            { <Outlet /> }
+          </div>
+        </div>
+
+        <Footer />
+
+    </div>
+  )
+}
+
+export default Educator
