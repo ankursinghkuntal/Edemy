a0dfa2938af9981b477280b63326d83590d87fbe
ankursinghkuntal <ankursinghkuntal361@gmail.com>
2025-02-25 08:30:02 +0530

    user correction

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -22,7 +22,7 @@ export const clerkWebhooks = async (req,res) => {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_Url,
+                    imageUrl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
@@ -31,9 +31,9 @@ export const clerkWebhooks = async (req,res) => {
                 
             case 'user.updated' : {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addreses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_Url,
+                    imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
